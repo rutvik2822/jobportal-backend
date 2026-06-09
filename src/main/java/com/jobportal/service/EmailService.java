e453183c5d32aa78e005b53763f0d64c0b85366d
rutvik2822 <rutvikdevdare@gmail.com>
2026-06-09 17:27:39 +0530

    Added email debug logs

--- src/main/java/com/jobportal/service/EmailService.java
+++ src/main/java/com/jobportal/service/EmailService.java
@@ -13,14 +13,21 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
-    public void sendEmail(String to, String subject, String text) {
+    public void sendEmail(String to,
+                          String subject,
+                          String text) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        System.out.println("Sending email to: " + to);
+
+        SimpleMailMessage message =
+                new SimpleMailMessage();
 
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
 
         mailSender.send(message);
+
+        System.out.println("Email sent successfully");
     }
 }
